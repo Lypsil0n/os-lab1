e9666ee3d2dac843a0ca58f3a380598474eb07df
HotYummy <bastianliljefors@gmail.com>
2024-11-11 14:18:54 +0000

    :(

--- Part1_code/shmem.c
+++ Part1_code/shmem.c
@@ -5,12 +5,23 @@
 #include <sys/shm.h>
 #include <stdlib.h>
 #include <time.h>
+#include <semaphore.h>
+#include <fcntl.h>
 #define SHMSIZE 128
 #define SHM_R 0400
 #define SHM_W 0200
 
+const char *sender_name = "sender";
+const char *reciever_name = "reciever";
+
 int main(int argc, char **argv)
 {
+	sem_t *sem_sender = sem_open(sender_name, O_CREAT, O_RDWR, 1);
+	    if (sem_sender == SEM_FAILED) {
+        printf("ojojoj :(");
+    }
+	sem_t *sem_reciever = sem_open(reciever_name, O_CREAT, O_RDWR, 0);
+	int status;
 	struct shm_struct {
 		int buffer[10];
 		int write_index;
@@ -34,31 +45,41 @@ int main(int argc, char **argv)
 		/* here's the parent, acting as producer */
 		while (var1 < 100) {
 			/* write to shmem */
-			while ((shmp->write_index + 1) % 10 == shmp->read_index); /* busy wait until the buffer is empty */
+			 /* busy wait until the buffer is empty */
 			for(int i = 0; i < 10; i++){
+				sem_wait(sem_sender);
 				delay = 100000 + rand() % 500000;
 				usleep(delay);
 				var1++;
 				printf("Sending %d\n", var1); fflush(stdout);
 				shmp->buffer[shmp->write_index] = var1;
 				shmp->write_index = (shmp->write_index + 1) % 10;
+				sem_post(sem_reciever);
 			}
 		}
+		sem_close(sem_sender);
+		sem_close(sem_reciever);
+		wait(&status);
+		sem_unlink(sender_name);
+		sem_unlink(reciever_name);
 		shmdt(addr);
 		shmctl(shmid, IPC_RMID, shm_buf);
 	} else {
 		/* here's the child, acting as consumer */
 		while (var2 < 100) {
-			/* read from shmem */
-			while ((shmp->read_index + 1) % 10 == shmp->write_index); /* busy wait until there is something */
+			/* read from shmem */ /* busy wait until there is something */
 			for(int i = 0; i < 10; i++){
+				sem_wait(sem_reciever);
 				delay = 20000 + rand() % 2000000;
 				usleep(delay);
 				var2 = shmp->buffer[shmp->read_index];
 				shmp->read_index = (shmp->read_index + 1) % 10;
 				printf("Received %d\n", var2); fflush(stdout);
+				sem_post(sem_sender);
 			}
 		}
+		sem_close(sem_sender);
+		sem_close(sem_reciever);
 		shmdt(addr);
 		shmctl(shmid, IPC_RMID, shm_buf);
 	}
